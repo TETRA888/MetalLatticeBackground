e1039ab285c15b5ea698a11662bd6b02fd2301fe
Asad <tetragamedevelopment@gmail.com>
2024-08-13 21:02:10 -0700

    optimized code and produced render

--- MetalBalls.py
+++ MetalBalls.py
@@ -4,15 +4,23 @@ import bpy
 # math for sine functions
 import math
 
-grid_size = 20
+grid_size = 200
+off_center = 2
+scale = 0.125
+z_scale = 4
 
 # Adding the sphere
-def create_sphere(current_location):
-    bpy.ops.mesh.primitive_uv_sphere_add(location = current_location)
+def create_sphere():
+    bpy.ops.mesh.primitive_uv_sphere_add()
     bpy.ops.object.shade_smooth()
+    return bpy.context.active_object
+
+current_sphere = create_sphere()
 
 # Positioning the sphere in the correct location
 for x in range(0,grid_size):
     for y in range(0,grid_size):
-        create_sphere(current_location = (x*2,y*2, math.sin(y) + math.cos(x))
+        copy_sphere = current_sphere.copy()
+        copy_sphere.location = (x*off_center,y*off_center, (math.sin(scale*y) + math.cos(scale*x))*z_scale)
+        bpy.context.collection.objects.link(copy_sphere)
     
